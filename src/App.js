1f6080a3476bc7d5c48f44c92e71f51fc267d745
Luiz Fellype <dylon.cassagoo@gmail.com>
2018-07-07 11:37:34 -0300

    feat: UI Mock

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Sidep from './components/sidePlayer'
-import { attack, eat } from './game/game'
+import './App.css'
+//import { attack, eat } from './game/game'
 const initialState = {
   fighter: {
     life: 10,
@@ -19,41 +19,63 @@ class App extends Component {
 
   state = {...initialState}
 
-  eatFighter = (whoate) => {
-    this.setState({fighter: eat(whoate)})
-  }
+  // eatFighter = (whoate) => {
+  //   this.setState({fighter: eat(whoate)})
+  // }
 
-  attackOpponent = (shooter, target) => {
-    this.setState({opponent: attack(shooter, target)})
-  }
+  // attackOpponent = (shooter, target) => {
+  //   this.setState({opponent: attack(shooter, target)})
+  // }
 
-  eatOpponent = (whoate) => {
-    this.setState({opponent: eat(whoate)})
-  }
+  // eatOpponent = (whoate) => {
+  //   this.setState({opponent: eat(whoate)})
+  // }
 
-  attackFighter = (shooter, target) => {
-    this.setState({fighter: attack(shooter, target)})
-  }
+  // attackFighter = (shooter, target) => {
+  //   this.setState({fighter: attack(shooter, target)})
+  // }
 
   render() {
     return (
       <React.Fragment>
-        <div >
-          <h3>LIFE: <Sidep value={this.state.fighter.life}/></h3>
-          <h3>DAMAGE: <Sidep value={this.state.fighter.damage}/></h3>
-          <h3>SKILL: <Sidep value={this.state.fighter.skill}/></h3>
-          <h3>DODGE: <Sidep value={this.state.fighter.dodge}/></h3> 
-          <button onClick={() => this.eatFighter(this.state.fighter)}>EAT</button> 
-          <button onClick={() => this.attackOpponent(this.state.fighter, this.state.opponent)}>ATTACK</button>
+        <div className='display'> 
+          <h3> TURN: Player 1</h3>
         </div>
-        <div >
-        <h3>LIFE: <Sidep value={this.state.opponent.life}/></h3>
-          <h3>DAMAGE: <Sidep value={this.state.opponent.damage}/></h3>
-          <h3>SKILL: <Sidep value={this.state.opponent.skill}/></h3>
-          <h3>DODGE: <Sidep value={this.state.opponent.dodge}/></h3> 
-          <button onClick={() => this.eatOpponent(this.state.opponent)}>EAT</button> 
-          <button onClick={() => this.attackFighter(this.state.opponent, this.state.fighter)}>ATTACK</button>
+        <div className='display'>
+          <div className=''> 
+            <h3> Player 1</h3>
+            <h3>LIFE: <div value={this.state.fighter.life}/></h3>
+            <h3>DAMAGE: <div value={this.state.fighter.damage}/></h3>
+            <h3>SKILL: <div value={this.state.fighter.skill}/></h3>
+            <h3>DODGE: <div value={this.state.fighter.dodge}/></h3> 
+            <button>EAT</button> 
+            <button>ATTACK</button>
+            {/* <button onClick={() => this.eatFighter(this.state.fighter)}>EAT</button> 
+            <button onClick={() => this.attackOpponent(this.state.fighter, this.state.opponent)}>ATTACK</button> */}
+          </div>
+        
+          <div class='sideL'>
+            <h3> Player 2</h3>
+            <h3>LIFE: <div value={this.state.opponent.life}/></h3>
+            <h3>DAMAGE: <div value={this.state.opponent.damage}/></h3>
+            <h3>SKILL: <div value={this.state.opponent.skill}/></h3>
+            <h3>DODGE: <div value={this.state.opponent.dodge}/></h3> 
+            <button>EAT</button> 
+            <button>ATTACK</button>
+            {/* <button onClick={() => this.attackFighter(this.state.opponent, this.state.fighter)}>ATTACK</button>
+            <button onClick={() => this.eatOpponent(this.state.opponent)}>EAT</button>  */}
+          </div>
         </div>
+        <div className='display'> 
+          <div>
+            <h2>Battle Log</h2>
+            <h3>.....</h3>
+            <h3>.....</h3>
+            <h3>.....</h3>
+          </div>
+        </div>
+        
+
       </React.Fragment>
     );
   }
