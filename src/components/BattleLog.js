8911dace99c6fc5dd62ca9bdba3cf92281f10965
Luiz Fellype <dylon.cassagoo@gmail.com>
2018-08-08 19:02:55 -0300

    feat: increase a list of message in battle log section

--- src/components/BattleLog.js
+++ src/components/BattleLog.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import Display from './Display'
+// import Display from './Display'
+import ToList from './toList'
 
 export default ({ status }) =>
     <div>
         <h2>Battle Log</h2>
-        <Display label="Player 1" value={status.msg1}/>
-        <Display label="Player 2" value={status.msg2}/>
+        {/* <Display label="Player 1" value={status.msg1}/> */}
+        {/* <Display label="Player 2" value={status.msg2}/> */}
+
+        <ToList logHistory={['Se Fodeu', ' Comi, tu n me mata', ' Ta Fodido']} />
+
     </div>
