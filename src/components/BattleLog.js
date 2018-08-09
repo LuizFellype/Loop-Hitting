f810af62b144355bd81565bf1088f8fd3ed98e4f
Luiz Fellype <dylon.cassagoo@gmail.com>
2018-08-09 15:25:25 -0300

    feat: icrease the time of the game and componentize him

--- src/components/BattleLog.js
+++ src/components/BattleLog.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import FormatList from './FormatList'
+import { threeFirst, time } from '../game/game'
 
-export default ({ status }) =>
+const date = new Date()
+const intDay = date.getDay()
+const daysOfWeek = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sabado']
+
+export default class BattleLog extends PureComponent {
+    state = {
+        arrOfTheToday: daysOfWeek[intDay].split('')
+    }
+
+    render(){
+    return (
     <div>
-        <h2>Battle Log</h2>
-        <FormatList logHistory={status.historyLog} />
+        <div>
+            <h4>{threeFirst(this.state.arrOfTheToday)} {time()}</h4>  
+            <h2>Battle Log</h2>
+        </div>
+        <FormatList logHistory={this.props.status.historyLog} />
     </div>
+    )
+}
+}
